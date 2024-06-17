757bc521d34b03732938bee5d90faa2cde3c3d22
Mbongeni Mlotha <61387136+Mbongeni01@users.noreply.github.com>
2024-06-17 23:58:12 +0200

    Delete LearningHeuristics directory

--- LearningHeuristics/heuristics/heuristic.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Heuristic(ABC):
-    """
-    This class
-    """
-    @abstractmethod
-    def estimate(self, state):
-        pass
--- LearningHeuristics/heuristics/zero_heuristic.py
+++
@@ -1,6 +0,0 @@
-from heuristics.heuristic import Heuristic
-
-
-class ZeroHeuristic(Heuristic):
-    def estimate(self, state):
-        return 0
